e299e82e88c60ca34cb130550877c706e69bc171
ryan9487 <ryan94879487@gmail.com>
2018-12-17 21:16:17 +0800

    秀出目錄 按enter進遊戲

--- index.cpp
+++ index.cpp
@@ -28,6 +28,9 @@
 #include <windows.h>
 #endif
 
+#include<iomanip>
+using namespace std;
+
 const int JUMP_HEIGHT = 3;
 
 char ProcessStdin()
@@ -212,8 +215,222 @@ int game(stage stage1, std::shared_ptr<Character::Character> mario){
     return 1;
 }
     
+void SetColor(int f=15,int b=0)
+{
+    unsigned short ForeColor=f+16*b;
+    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hCon,ForeColor);
+}
+
+class TITLE{
+    public:
+        TITLE(){
+            SetColor(2,0);
+            cout<<"*************************************************************************************************************************************************************\n";
+            cout<<setw(118)<<"*****   *   *   *****   *****   *****      *   *     *     *****   *****   *****\n";
+            cout<<setw(118)<<"*       *   *   *   *   *       *   *      ** **    * *    *   *     *     *   *\n";
+            cout<<setw(118)<<"*****   *   *   *****   *****   *****      * * *   *****   *****     *     *   *\n";
+            cout<<setw(118)<<"    *   *   *   *       *       *  *       *   *   *   *   *  *      *     *   *\n";
+            cout<<setw(118)<<"*****   *****   *       *****   *   *      *   *   *   *   *   *   *****   *****\n";
+            cout<<"\n\n\n\n";
+    
+            SetConsoleOutputCP(437);
+            char block=254;
+            SetColor(1,0);
+            cout<<setw(45)<<block;
+            cout<<" Control : Using A D on Keyboard to Move the Character and W to Jump\n";
+            cout<<"\n\n\n\n";
+    
+            SetColor(14,0);
+            cout<<setw(95)<<"Press Enter to Start the Game...\n";
+    
+
+            SetColor(0.0);}
+};
+
+class IMAGE{
+    public:
+        char b=254; //brick
+        IMAGE(){
+            SetColor(0,0);
+            cout<<b<<b<<b<<b<<b;
+            SetColor(12,0);
+            cout<<b<<b<<b<<b<<b;                        //1st row
+            
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b<<b;
+            SetColor(12,0);
+            cout<<b<<b<<b<<b<<b<<b<<b<<b<<b;          //2nd row
+            
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b<<b;
+            SetColor(14,0);
+            cout<<b<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b;
+            SetColor(5,0);
+            cout<<b;
+            SetColor(7,0);
+            cout<<b;                                 //3rd row
+            
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b;
+            SetColor(14,0);
+            cout<<b;
+            SetColor(7,0);
+            cout<<b;
+            SetColor(14,0);
+            cout<<b;
+            SetColor(7,0);
+            cout<<b<<b<<b;
+            SetColor(5,0);
+            cout<<b;
+            SetColor(7,0);
+            cout<<b<<b<<b;                     //4th row
+            
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b;
+            SetColor(14,0);
+            cout<<b;
+            SetColor(7,0);
+            cout<<b;
+            SetColor(14,0);
+            cout<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b<<b;
+            SetColor(5,0);
+            cout<<b;
+            SetColor(7,0);
+            cout<<b<<b<<b;               //5th row
+            
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b;
+            SetColor(14,0);
+            cout<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b<<b<<b;
+            SetColor(5,0);
+            cout<<b<<b<<b<<b;                  //6th row
+            
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b<<b<<b<<b<<b<<b;           //7th row
+           
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b<<b;
+            SetColor(12,0);
+            cout<<b<<b;
+            SetColor(1,0);
+            cout<<b;
+            SetColor(12,0);
+            cout<<b<<b;
+            SetColor(1,0);
+            cout<<b;
+            SetColor(12,0);
+            cout<<b;             //8th row
+
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b;
+            SetColor(12,0);
+            cout<<b<<b<<b;
+            SetColor(1,0);
+            cout<<b;
+            SetColor(12,0);
+            cout<<b<<b;
+            SetColor(1,0);
+            cout<<b;
+            SetColor(12,0);
+            cout<<b<<b<<b;         //9th row
+
+            SetColor(0,0);
+            cout<<"\n"<<b<<b;
+            SetColor(12,0);
+            cout<<b<<b<<b<<b;
+            SetColor(1,0);
+            cout<<b<<b<<b<<b;
+            SetColor(12,0);
+            cout<<b<<b<<b<<b;           //10th row
+
+            SetColor(0,0);
+            cout<<"\n"<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b;
+            SetColor(12,0);
+            cout<<b;
+            SetColor(1,0);
+            cout<<b;
+            SetColor(14,0);
+            cout<<b;
+            SetColor(1,0);
+            cout<<b<<b;
+            SetColor(14,0);
+            cout<<b;
+            SetColor(1,0);
+            cout<<b;
+            SetColor(12,0);
+            cout<<b;
+            SetColor(7,0);
+            cout<<b<<b;           //11th row
+
+            SetColor(0,0);
+            cout<<"\n"<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b<<b;
+            SetColor(1,0);
+            cout<<b<<b<<b<<b<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b<<b;        //12th row
+
+            SetColor(0,0);
+            cout<<"\n"<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b;
+            SetColor(1,0);
+            cout<<b<<b<<b<<b<<b<<b<<b<<b;
+            SetColor(7,0);
+            cout<<b<<b;        //13th row
+
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b<<b;
+            SetColor(1,0);
+            cout<<b<<b<<b;
+            SetColor(0,0);
+            cout<<b<<b;
+            SetColor(1,0);
+            cout<<b<<b<<b;           //14th row
+
+            SetColor(0,0);
+            cout<<"\n"<<b<<b<<b;
+            SetColor(8,0);
+            cout<<b<<b<<b;
+            SetColor(0,0);
+            cout<<b<<b<<b<<b;
+            SetColor(8,0);
+            cout<<b<<b<<b;           //15th row
+
+            SetColor(0,0);
+            cout<<"\n"<<b<<b;
+            SetColor(8,0);
+            cout<<b<<b<<b<<b;
+            SetColor(0,0);
+            cout<<b<<b<<b<<b;
+            SetColor(8,0);
+            cout<<b<<b<<b<<b;           //15th row
+
+
+
+         }
+};
+
 
 int main(){
+
+        TITLE();
+        IMAGE();
+        cin.get();
+        
+        
         setvbuf(stdout, nullptr, _IOFBF, 1000);
         SetConsoleOutputCP(CP_UTF8);
 
@@ -225,7 +442,10 @@ int main(){
         auto stage1 = readMap("./stages/stage 1-1.json");
         auto mario = std::make_shared<Character::Character>("Mario");
 
-        clear();
-        int result = game(stage1, mario);
-        return 0;
+         clear();
+
+        
+         int result = game(stage1, mario);
+         return 0;
+        
 }
